56a50d625133276f031fc0ace5a241032a551581
AbuBakkar32 <abubakkar.swe@gmail.com>
2022-06-18 20:30:16 +0600

    README file changed

--- src/app/setting/setting.component.ts
+++ src/app/setting/setting.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent} from "../app.component";
-import { GlobalConstants } from "../common/global-constants";
 
 @Component({
   selector: 'app-setting',
