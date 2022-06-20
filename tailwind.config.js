3ad4f7e61c6ae77aa5f53ff3d31833db09b0dd60
Abu Bakkar Siddik <44539099+AbuBakkar32@users.noreply.github.com>
2022-06-20 14:48:54 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
+  future: {
+    defaultLineHeights: true,
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  experimental: {
+    additionalBreakpoint: true,
+    extendedFontSizeScale: true,
+    extendedSpacingScale: true,
+  },
   theme: {
     extend: {},
   },
